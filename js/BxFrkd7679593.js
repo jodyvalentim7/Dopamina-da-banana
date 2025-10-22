19b38491b30c32b932e50fca97caca487d9d0e32
jodyvalentim7 <jody.empresas@gmail.com>
2025-10-22 13:04:13 +0100

    Add files via upload

---
+++ js/BxFrkd7679593.js
@@ -0,0 +1,1 @@
+function runDelayedFunctions(e){try{document.querySelectorAll(".atomicat-delay").forEach((e=>e.classList.remove("atomicat-delay"))),e?.setDisplayed&&localStorage.setItem(e?.setDisplayed,!0)}catch(e){console.log(e)}}
